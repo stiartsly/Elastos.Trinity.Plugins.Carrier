026a35a581906663ad50c80329b63bda99085383
snackwang <41035012+snackwangpetter@users.noreply.github.com>
2019-10-25 16:55:53 +0800

    Resolve issue about being unable to find base58 for android (#4)

--- src/android/Base58.java
+++ src/android/Base58.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Elastos Foundation
+ * Copyright (c) 2018 Elastos Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,28 +22,145 @@
 
 package org.elastos.trinity.plugins.carrier;
 
-public class Base58 {
-    /**
-     * Encodes the given bytes as a base58 string (no checksum is appended).
-     *
-     * @param input the bytes to encode
-     * @return the base58-encoded string
-     */
+class Base58 {
+    private static final char[] ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".toCharArray();
+    private static final int BASE_58 = ALPHABET.length;
+    private static final int BASE_256 = 256;
+
+    private static final int[] INDEXES = new int[128];
+
+    static {
+        for (int i = 0; i < INDEXES.length; i++) {
+            INDEXES[i] = -1;
+        }
+        for (int i = 0; i < ALPHABET.length; i++) {
+            INDEXES[ALPHABET[i]] = i;
+        }
+    }
+
     public static String encode(byte[] input) {
-    	return org.elastos.trinity.plugins.carrier.base58.Base58.base58Encode(input);
+        if (input.length == 0)
+            return "";
+
+        // Make a copy of the input since we are going to modify it.
+        input = copyOfRange(input, 0, input.length);
+
+        // Count leading zeroes
+        int zeroCount = 0;
+        while (zeroCount < input.length && input[zeroCount] == 0) {
+            ++zeroCount;
+        }
+
+        // The actual encoding
+        byte[] temp = new byte[input.length * 2];
+        int j = temp.length;
+
+        int startAt = zeroCount;
+        while (startAt < input.length) {
+            byte mod = divmod58(input, startAt);
+            if (input[startAt] == 0) {
+                ++startAt;
+            }
+
+            temp[--j] = (byte) ALPHABET[mod];
+        }
+
+        // Strip extra '1' if any
+        while (j < temp.length && temp[j] == ALPHABET[0]) {
+            ++j;
+        }
+
+        // Add as many leading '1' as there were leading zeros.
+        while (--zeroCount >= 0) {
+            temp[--j] = (byte) ALPHABET[0];
+        }
+
+        byte[] output = copyOfRange(temp, j, temp.length);
+        return new String(output);
     }
 
-    /**
-     * Decodes the given base58 string into the original data bytes.
-     *
-     * @param input the base58-encoded string to decode
-     * @return the decoded data bytes
-     * @throws IllegalArgumentException if the given string is not a valid base58 string
-     */
-    public static byte[] decode(String input) throws IllegalArgumentException {
-    	return org.elastos.trinity.plugins.carrier.base58.Base58.base58Decode(
-    			input.subSequence(0, input.length()));
+    public static byte[] decode(String input) {
+        if (input.length() == 0)
+            return new byte[0];
+
+        byte[] input58 = new byte[input.length()];
+
+        // Transform the String to a base58 byte sequence
+        for (int i = 0; i < input.length(); ++i) {
+            char c = input.charAt(i);
+
+            int digit58 = -1;
+            if (c >= 0 && c < 128) {
+                digit58 = INDEXES[c];
+            }
+            if (digit58 < 0) {
+                throw new RuntimeException("Not a Base58 input: " + input);
+            }
+
+            input58[i] = (byte) digit58;
+        }
+
+        // Count leading zeroes
+        int zeroCount = 0;
+        while (zeroCount < input58.length && input58[zeroCount] == 0) {
+            ++zeroCount;
+        }
+
+        // The encoding
+        byte[] temp = new byte[input.length()];
+        int j = temp.length;
+
+        int startAt = zeroCount;
+        while (startAt < input58.length) {
+            byte mod = divmod256(input58, startAt);
+            if (input58[startAt] == 0) {
+                ++startAt;
+            }
+
+            temp[--j] = mod;
+        }
+
+        // Do no add extra leading zeroes, move j to first non null byte.
+        while (j < temp.length && temp[j] == 0) {
+            ++j;
+        }
+
+        return copyOfRange(temp, j - zeroCount, temp.length);
     }
 
+    private static byte divmod58(byte[] number, int startAt) {
+        int remainder = 0;
+        for (int i = startAt; i < number.length; i++) {
+            int digit256 = (int) number[i] & 0xFF;
+            int temp = remainder * BASE_256 + digit256;
+
+            number[i] = (byte) (temp / BASE_58);
+
+            remainder = temp % BASE_58;
+        }
+
+        return (byte) remainder;
+    }
+
+    private static byte divmod256(byte[] number58, int startAt) {
+        int remainder = 0;
+        for (int i = startAt; i < number58.length; i++) {
+            int digit58 = (int) number58[i] & 0xFF;
+            int temp = remainder * BASE_58 + digit58;
+
+            number58[i] = (byte) (temp / BASE_256);
+
+            remainder = temp % BASE_256;
+        }
+
+        return (byte) remainder;
+    }
+
+    private static byte[] copyOfRange(byte[] source, int from, int to) {
+        byte[] range = new byte[to - from];
+        System.arraycopy(source, from, range, 0, range.length);
+
+        return range;
+    }
 }
 
